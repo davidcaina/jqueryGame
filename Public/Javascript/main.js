c0fa4736709fc9a50cc0c9c190d9454106b1ed8b
David Cainã <davidcaina@hotmail.com>
2020-07-27 14:39:22 -0300

    brincando com popup.

--- Public/Javascript/main.js
+++ Public/Javascript/main.js
@@ -3,80 +3,41 @@ var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 
 
+
 // Main Functions:
 
 // Init Function
+
 $(function() {
-    atualizaTamanhoFrase();
-    inicializaContadores();
-    inicializaCronometro();
-    inicializaMarcadores();
-    $("#botao-reiniciar").click(reiniciaJogo);
+    abrePopupTutorial();
+    fechaPopupTutorial();
 });
 
-function atualizaTamanhoFrase() {
-    var frase = $(".frase").text();
-    var numPalavras  = frase.split(" ").length;
-    var tamanhoFrase = $("#tamanho-frase");
-    tamanhoFrase.text(numPalavras);
-}
+function abrePopupTutorial(){
 
-function inicializaContadores() {
-    campo.on("input", function() {
-        var conteudo = campo.val();
-
-        var qtdPalavras = conteudo.split(/\S+/).length - 1;
-        $("#contador-palavras").text(qtdPalavras);
-
-        var qtdCaracteres = conteudo.length;
-        $("#contador-caracteres").text(qtdCaracteres);
+    $("#btn-tutorial").click(function(){
+        console.log("Teste");
+        $("#main-div").toggleClass("custom-blur");
+        $("#popup-tutorial").toggleClass("popup-desativado");
     });
 }
 
-function inicializaMarcadores() {
-    var frase = $(".frase").text();
-    campo.on("input", function() {
-        var digitado = campo.val();
-        var comparavel = frase.substr(0, digitado.length);
+function fechaPopupTutorial(){
+    $(document).mouseup(function(e){
 
-        if (digitado == comparavel) {
-            campo.addClass("borda-verde");
-            campo.removeClass("borda-vermelha");
-        } else {
-            campo.addClass("borda-vermelha");
-            campo.removeClass("borda-verde");
+        var container = $("#popup-tutorial");
+        
+        if(!container.hasClass("popup-desativado")){
+
+            if ((!container.is(e.target) && container.has(e.target).length === 0)){
+                $("#popup-tutorial").toggleClass("popup-desativado");
+                $("#main-div").toggleClass("custom-blur");
+            }
+          
+            $("#close-popup-tutorial").click(function(){
+                $("#popup-tutorial").toggleClass("popup-desativado");
+                $("#main-div").toggleClass("custom-blur");
+            });
         }
     });
-}
-
-function inicializaCronometro() {
-    var tempoRestante = $("#tempo-digitacao").text();
-    campo.one("focus", function() {
-    	var cronometroID = setInterval(function() {
-    		tempoRestante--;
-    		$("#tempo-digitacao").text(tempoRestante);
-    		if (tempoRestante < 1) {
-                clearInterval(cronometroID);
-                finalizaJogo();
-    		}
-    	}, 1000);
-    });
-}
-
-function finalizaJogo() {
-    campo.attr("disabled", true);
-    campo.toggleClass("campo-desativado");
-    inserePlacar();
-}
-
-function reiniciaJogo() {
-    campo.attr("disabled", false);
-    campo.val("");
-    $("#contador-palavras").text(0);
-    $("#contador-caracteres").text(0);
-    $("#tempo-digitacao").text(tempoInicial);
-    inicializaCronometro();
-    campo.toggleClass("campo-desativado");
-    campo.removeClass("borda-vermelha");
-    campo.removeClass("borda-verde");
 }
