2c4a0b4b9b0e4fa1ffaca3d2341fb18e9de265c0
David Cainã Araujo Vieira <davidcaina@hotmail.com>
2020-09-01 18:58:28 -0300

    Making facade text.

--- Public/Javascript/text-controller.js
+++ Public/Javascript/text-controller.js
@@ -6,6 +6,7 @@ On page loading
 $(function() {
     $("#texto-digitar").load('/Files/text/extremo/extremo.txt', function(text) {
         $(this).html(text);
+        $("#currentResponse").html(text);
         verifySpecialWords();
         atualizaTamanhoFrase();
         inicializaMarcadores();
@@ -29,6 +30,7 @@ $(document).on('input', "#difficulty-level-range", function () {
 
         $("#texto-digitar").load('/Files/text/facil/facil.txt', function(text) {
             $(this).html(text);
+            $("#currentResponse").html(text);
             verifySpecialWords();
             atualizaTamanhoFrase();
             reiniciaJogo();
@@ -38,6 +40,7 @@ $(document).on('input', "#difficulty-level-range", function () {
     else if(this.value == 4){
         $("#texto-digitar").load('/Files/text/medio/medio.txt', function(text) {
             $(this).html(text);
+            $("#currentResponse").html(text);
             verifySpecialWords();
             atualizaTamanhoFrase();
             reiniciaJogo();
@@ -47,6 +50,7 @@ $(document).on('input', "#difficulty-level-range", function () {
     else if(this.value == 6){
         $("#texto-digitar").load('/Files/text/dificil/dificil.txt', function(text) {
             $(this).html(text);
+            $("#currentResponse").html(text);
             verifySpecialWords();
             atualizaTamanhoFrase();
             inicializaMarcadores();
@@ -55,6 +59,7 @@ $(document).on('input', "#difficulty-level-range", function () {
     else if(this.value == 8){
         $("#texto-digitar").load('/Files/text/extremo/extremo.txt', function(text) {
             $(this).html(text);
+            $("#currentResponse").html(text);
             verifySpecialWords();
             atualizaTamanhoFrase();
             inicializaMarcadores();
@@ -68,46 +73,66 @@ Função responsavel por realizar a alteração das cores para as palavras espec
 */
  function verifySpecialWords(){
     var texto = $("#texto-digitar").html();
+    var currentResponse = texto;
     palavrasDoTexto = texto.trim().split(/[\s,]+/);
     for(i = 0; i < palavrasDoTexto.length; i++){
         
         var currentText = palavrasDoTexto[i];
         if(currentText.startsWith("{!")){ // escrever primeira letra maiscula.
+            
             var currentIndex = texto.indexOf("{!");
             var currentLenght = currentText.length;
-            var newWord = 'teste';
-            texto = texto.substring(0, currentIndex) + '<span class=\"texto-firstMaiscula\" href="#" data-toggle="tooltip" data-placement="top" title="Hooray!">' + newWord + '</span>' + texto.substring(currentIndex + currentLenght, texto.length-1);
+            var newWord = currentText.substring(2, currentLenght-1);
+            var firstLetterupperCaseWord = newWord.charAt(0).toUpperCase() + newWord.slice(1)
+
+            currentResponse = currentResponse.substring(0, currentIndex) + firstLetterupperCaseWord + currentResponse.substring(currentIndex + currentLenght, currentResponse.length);
+            texto = texto.substring(0, currentIndex) + '<span class=\"texto-firstMaiscula\" href="#" data-toggle="tooltip" data-placement="top" title="Resposta: '+ firstLetterupperCaseWord +' ">' + newWord + '</span>' + texto.substring(currentIndex + currentLenght, texto.length);   
         }
         
-        else if(currentText.startsWith("{&")){ // escrever toda a palavra maiscula.
-            var currentIndex = texto.indexOf("{&");
+        else if(currentText.startsWith("{-")){ // escrever toda a palavra maiscula.
+            var currentIndex = texto.indexOf("{-");
             var currentLenght = currentText.length;
-            var newWord = 'teste';
-            texto = texto.substring(0, currentIndex) + '<span class=\"texto-fullMaiscula\">teste2</span>' + texto.substring(currentIndex + currentLenght, texto.length-1);
+            var newWord = currentText.substring(2, currentLenght-1);
+            var allUpperCaseWord = newWord.toUpperCase();
+
+            currentResponse = currentResponse.substring(0, currentIndex) + allUpperCaseWord + currentResponse.substring(currentIndex + currentLenght, currentResponse.length); 
+            texto = texto.substring(0, currentIndex) + '<span class=\"texto-fullMaiscula\" href="#" data-toggle="tooltip" data-placement="top" title="Resposta: '+ allUpperCaseWord +'">' + newWord + '</span>'+texto.substring(currentIndex + currentLenght, texto.length);   
         }
 
         else if(currentText.startsWith("{#")){ // escrever a ultima letra maiscula.
             var currentIndex = texto.indexOf("{#");
             var currentLenght = currentText.length;
-            var newWord = 'teste';
-            texto = texto.substring(0, currentIndex) + '<span class=\"texto-lastMaiscula\">teste3 </span>' + texto.substring(currentIndex + currentLenght, texto.length-1);
+            var newWord = currentText.substring(2, currentLenght-1);
+            var lastLetterupperCaseWord = newWord.substring(0, newWord.length-1) + newWord.charAt(newWord.length-1).toUpperCase()
+            
+            currentResponse = currentResponse.substring(0, currentIndex) + lastLetterupperCaseWord +currentResponse.substring(currentIndex + currentLenght, currentResponse.length); 
+            texto = texto.substring(0, currentIndex) + '<span class=\"texto-lastMaiscula\" href="#" data-toggle="tooltip" data-placement="top" title="Resposta: '+ lastLetterupperCaseWord +' ">' + newWord + '</span>' + texto.substring(currentIndex + currentLenght, texto.length);   
         }
 
         else if(currentText.startsWith("{@")){ // repetir a palavra duas vezes.
             var currentIndex = texto.indexOf("{@");
             var currentLenght = currentText.length;
-            var newWord = 'teste';
-            texto = texto.substring(0, currentIndex) + '<span class=\"texto-double\">teste4 </span>' + texto.substring(currentIndex + currentLenght, texto.length-1);
+            var newWord = currentText.substring(2, currentLenght-1);
+            var doubleWord = newWord+newWord;
+            var newLen = doubleWord.length+3;
+
+            currentResponse = currentResponse.substring(0, currentIndex) + doubleWord + currentResponse.substring(currentIndex + newLen, currentResponse.length); 
+            texto = texto.substring(0, currentIndex) + '<span class=\"texto-double\" href="#" data-toggle="tooltip" data-placement="top" title="Resposta: '+ doubleWord +' ">' + newWord + '</span>' + texto.substring(currentIndex + currentLenght, texto.length);   
         }
 
         else if(currentText.startsWith("{%")){ // escrever palavra inversa.
             var currentIndex = texto.indexOf("{%");
             var currentLenght = currentText.length;
-            var newWord = 'teste';
-            texto = texto.substring(0, currentIndex) + '<span class=\"texto-inverse\">teste5 </span>' + texto.substring(currentIndex + currentLenght, texto.length-1);
+            var newWord = currentText.substring(2, currentLenght-1);
+            var reverseWord = newWord.split('').reverse().join('');
+            console.log("{%:" + reverseWord);
+
+            currentResponse = currentResponse.substring(0, currentIndex) + reverseWord + currentResponse.substring(currentIndex + currentLenght, currentResponse.length); 
+            texto = texto.substring(0, currentIndex) + '<span class=\"texto-inverse\" href="#" data-toggle="tooltip" data-placement="top" title="Resposta: '+ reverseWord +' ">' + newWord + '</span>' + texto.substring(currentIndex + currentLenght, texto.length);   
         }
     }
     $("#texto-digitar").html(texto);
+    $("#currentResponse").html(currentResponse);
  }
 
 
